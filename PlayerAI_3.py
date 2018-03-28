ef5a8aefe0562e53ca0039739b41e32de33d6afa
Drew Dakin <dakindre4@gmail.com>
2018-03-28 15:45:33 -0400

    Updated Player File with Basic Min Max

--- PlayerAI_3.py
+++ PlayerAI_3.py
@@ -7,13 +7,13 @@ from Displayer_3 import Displayer
 from copy import deepcopy
 import queue
 
-finalDepth = 2
+finalDepth = 4
 
 class PlayerAI(BaseAI):
     
     def getMove(self, grid):
-        alpha = scoreObject(float('-inf'), None)
-        beta = scoreObject(float('inf'), None)
+        alpha = maxMinObjects('alpha', float('-inf'), None)
+        beta = maxMinObjects('beta', float('inf'), None)
 
         #print('alpha ', alpha.value)
         #print('beta ', beta.value)
@@ -21,15 +21,16 @@ class PlayerAI(BaseAI):
         #newGrid = grid.clone()
         #maxScore = self.alphaBeta(grid, alpha, beta, 0, None, 'max', grid)
         maxScore = self.maximize(grid, alpha, beta, 0, None, grid)
-        
-        return maxScore.getChild()
+        #print('Return Type ', maxScore.getType(), ' Max Score ', maxScore.getScore(), 'Move ', maxScore.getMove())
+
+        return maxScore.getMove()
         
         
         
 
     def alphaBeta(self, grid, depth):
         
-        if depth > finalDepth: return True
+        if depth == finalDepth: return True
 
         #No moves to make exit with game over
         if not grid.canMove(): return True
@@ -39,67 +40,60 @@ class PlayerAI(BaseAI):
         
     
     def maximize(self, grid, alpha, beta, depth, move, parent):
+        #print('Current Move ', move)
         if self.alphaBeta(grid, depth):
             heuristic = self.eval(grid)
-            return maxMinObjects(move, heuristic)
+            return maxMinObjects('max-heur', heuristic, move)
         
         moves = grid.getAvailableMoves()
-        ma = maxMinObjects(None, float('-inf'))
+        ma = maxMinObjects('ma', float('-inf'), None)
         
         for x in moves:
                            
             newGrid = grid.clone()
             newGrid.move(x)
-
             #print('Maximize ')
-            #print('alpha [', alpha.value, '] beta [', beta.value, '] depth [', depth, ']')
+            #print('maxScore [', ma.score, '] depth [', depth, ']', ' move [', move, ']')
+            #print('alpha [', alpha.score, '] beta [', beta.score, '] depth [', depth, ']', ' move [', move, ']')
             #Displayer().display(newGrid)
             
             mi = self.minimize(newGrid, alpha, beta, depth+1, x, grid)
 
-            if mi.getUtility() > ma.getUtility():
-                ma.child = mi.getChild()
-                ma.utility = mi.getUtility()
-
-            if ma.getUtility() >= beta.getValue():
-                break
-
-            if ma.getUtility() > alpha.getValue():
-                alpha.value = ma.getUtility()
-                alpha.move = ma.getChild
-
+            if mi.getScore() > ma.getScore():
+                #print('INMAX---Max Returned Score ', mi.getScore(), ' Min Score ',ma.getScore())
+                ma.score = mi.getScore()
+                ma.move = x
+            
         return ma
                                
                 
     def minimize(self, grid, alpha, beta, depth, move, parent):
+        #print('Current Move ', move)
         if self.alphaBeta(grid, depth):
             heuristic = self.eval(grid)
-            return maxMinObjects(move, heuristic)
+            return maxMinObjects('min-heur', heuristic, move)
         
-        mi = maxMinObjects(None, float('inf'))
-        #print('In Min')
+        mi = maxMinObjects('mi', float('inf'), None)
+
         cells = grid.getAvailableCells()
         for xy in cells:
             
             newGrid = grid.clone()
             newGrid.insertTile(xy, 2)
-
             #print('Minimize ')
-            #print('alpha [', alpha.value, '] beta [', beta.value, '] depth [', depth, ']')
+            #print('minScore [', mi.score, '] depth [', depth, ']', ' move [', move, ']')
+            #print('alpha [', alpha.score, '] beta [', beta.score, '] depth [', depth, ']', ' move [', move, ']')
             #Displayer().display(newGrid)
+
             ma = self.maximize(newGrid, alpha, beta, depth+1, move, grid)
-
-            if ma.getUtility() < mi.getUtility():
-                mi.child = ma.getChild()
-                mi.utility = ma.getUtility()
-
-            if mi.getUtility() <= alpha.getValue():
-                break
-
-            if mi.getUtility() < beta.getValue():
-                beta.value = mi.getUtility()
-                beta.value = mi.getChild()
             
+
+            if ma.getScore() < mi.getScore():
+                #print('INMIN---Min Returned Score ', ma.getScore(), ' Max Score ',mi.getScore())
+                mi.score = ma.getScore()
+                mi.move = move
+
+
         return mi
 
 
@@ -112,32 +106,100 @@ class PlayerAI(BaseAI):
         
 
 
-class scoreObject:
-    def __init__(self, value, move):
-        self.value = value
+class maxMinObjects:
+    def __init__(self, typeAB, score, move):
+        self.typeAB = typeAB
+        self.score = score
         self.move = move
 
-    def getValue(self):
-        return self.value
+    def getType(self):
+        return self.typeAB
+
+    def getScore(self):
+        return self.score
 
     def getMove(self):
         return self.move
 
-class maxMinObjects:
-    def __init__(self, child, utility):
-        self.child = child
-        self.utility = utility
-
-    def getChild(self):
-        return self.child
-
-    def getUtility(self):
-        return self.utility
 
 
 
+'''if mi.getScore() > ma.getScore():
+
+    ma.score = mi.getScore()
+    ma.move = x
+
+if ma.getScore() > alpha.getScore():
+    alpha.score = ma.getScore()
+    alpha.move = ma.move
+
+if not beta.getScore() ==  float('inf'):
+    if ma.getScore() >= beta.getScore():
+        break'''
 
 
+                               
+
+'''if alpha.getScore() < mi.getScore():
+    alpha.score = mi.getScore()
+    alpha.move = x
+
+print(beta.getScore())
+if not beta.getScore() == float('inf'):
+    print(beta.getScore())
+    if beta.getScore() <= alpha.getScore():
+        print('Max End Reached')
+        break
+
+if mi.getUtility() > ma.getUtility():
+    ma.child = mi.getChild()
+    ma.utility = mi.getUtility()
+
+if ma.getUtility() >= beta.getValue():
+    break
+
+if ma.getUtility() > alpha.getValue():
+    alpha.value = ma.getUtility()
+    alpha.move = ma.getChild'''
+
+
+
+
+'''if ma.getScore() < mi.getScore():
+    
+    mi.score = ma.getScore()
+    mi.move = move
+
+if mi.getScore() <beta.getScore():
+    beta.score = mi.getScore()
+    #print('Beta Score ', beta.getScore())
+
+if mi.getScore() <= alpha.getScore():
+    break'''
+
+'''if beta.getScore() > ma.getScore():
+    print('Should Update')
+    beta.score = ma.getScore()
+
+if beta.getScore() <= alpha.getScore():
+    print('Min End Reached')
+    break'''
+
+'''#print('Max Utility ', ma.getUtility(), ' Min Utility ', mi.getUtility())
+if ma.getUtility() < mi.getUtility():
+    #print('ma.getUtility() < mi.getUtility()')
+    mi.utility = ma.getUtility()
+    print('Max Utility ', ma.getUtility(), ' Min Utility ', mi.getUtility())
+    mi.child = ma.getChild()
+
+if mi.getUtility() <= alpha.getValue():
+    #print('mi.getUtility() <= alpha.getValue()')
+    break
+
+if mi.getUtility() < beta.getValue():
+    #print('mi.getUtility() < beta.getValue()')
+    beta.value = mi.getUtility()
+    beta.move = mi.getChild()'''
 
 
 
